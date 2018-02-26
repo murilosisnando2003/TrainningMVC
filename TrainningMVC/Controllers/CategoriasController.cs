4406bac269401800e9a2f08b79eef59ee1d02490
Murilo Rodrigues <murilorodrigues@edgeglobalsupply.com.br>
2018-02-26 14:06:19 -0300

    go

--- TrainningMVC/Controllers/CategoriasController.cs
+++ TrainningMVC/Controllers/CategoriasController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrainningMVC.Models;
 
 namespace TrainningMVC.Controllers
 {
     public class CategoriasController : Controller
     {
+        public static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria()
+            {
+                CategoriaId = 1,
+                Nome="Notebooks"
+            },
+            new Categoria()
+            {
+                CategoriaId = 2,
+                Nome="Monitores"
+            },
+            new Categoria()
+            {
+                CategoriaId = 3,
+                Nome="Impressoras"
+            },
+            new Categoria()
+            {
+                CategoriaId = 4,
+                Nome="Mouses"
+            },
+            new Categoria()
+            {
+                CategoriaId = 5,
+                Nome="Desktops"
+            },
+
+
+        };
         // GET: Categorias
         public ActionResult Index()
         {
-            return View();
+            return View(categorias);
         }
     }
 }
