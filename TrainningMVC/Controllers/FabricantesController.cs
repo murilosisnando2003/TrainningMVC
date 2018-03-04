d3ae27066fc5d1794dde596ae72a54839baca714
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2018-03-04 13:00:24 -0300

    2

---
+++ TrainningMVC/Controllers/FabricantesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrainningMVC.Contexts;
+
+namespace TrainningMVC.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+    }
+}
