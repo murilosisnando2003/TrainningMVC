cc85ebe8cf438ab0854cb41316d6662e43e7f891
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2018-03-04 16:03:15 -0300

    3

--- TrainningMVC/Controllers/CategoriasController.cs
+++ TrainningMVC/Controllers/CategoriasController.cs
@@ -1,48 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using TrainningMVC.Contexts;
 using TrainningMVC.Models;
 
 namespace TrainningMVC.Controllers
 {
     public class CategoriasController : Controller
     {
-        public static IList<Categoria> categorias = new List<Categoria>()
-        {
-            new Categoria()
-            {
-                CategoriaId = 1,
-                Nome="Notebooks"
-            },
-            new Categoria()
-            {
-                CategoriaId = 2,
-                Nome="Monitores"
-            },
-            new Categoria()
-            {
-                CategoriaId = 3,
-                Nome="Impressoras"
-            },
-            new Categoria()
-            {
-                CategoriaId = 4,
-                Nome="Mouses"
-            },
-            new Categoria()
-            {
-                CategoriaId = 5,
-                Nome="Desktops"
-            },
+        private EFContext context = new EFContext();
+
+        //public static IList<Categoria> categorias = new List<Categoria>()
+        //{
+        //    new Categoria()
+        //    {
+        //        CategoriaId = 1,
+        //        Nome="Notebooks"
+        //    },
+        //    new Categoria()
+        //    {
+        //        CategoriaId = 2,
+        //        Nome="Monitores"
+        //    },
+        //    new Categoria()
+        //    {
+        //        CategoriaId = 3,
+        //        Nome="Impressoras"
+        //    },
+        //    new Categoria()
+        //    {
+        //        CategoriaId = 4,
+        //        Nome="Mouses"
+        //    },
+        //    new Categoria()
+        //    {
+        //        CategoriaId = 5,
+        //        Nome="Desktops"
+        //    },
 
 
-        };
+        //};
         // GET: Categorias
         public ActionResult Index()
         {
-            return View(categorias.OrderBy(c => c.Nome));
+            return View(context.Categorias.OrderBy(c => c.Nome));
         }
 
         public ActionResult Create()
@@ -55,15 +60,24 @@ namespace TrainningMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Categoria categoria)
         {
-            categorias.Add(categoria);
-            categoria.CategoriaId =
-                categorias.Select(m => m.CategoriaId).Max() + 1;
+            context.Categorias.Add(categoria);
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
 
         public ActionResult Edit(long id)
         {
-            return View(categorias.Where(m => m.CategoriaId ==id).First());
+            if (id == null)
+            {
+                return new
+                    HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
         }
 
 
@@ -71,27 +85,53 @@ namespace TrainningMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(Categoria categoria)
         {
-            categorias.Remove(categorias.Where(c => c.CategoriaId == c.CategoriaId).First());
-            categorias.Add(categoria);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                context.Entry(categoria).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
         }
 
-        public ActionResult Delete(long id)
+        public ActionResult Delete(long? id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(Categoria categoria)
+        public ActionResult Delete(long id)
         {
-            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            Categoria categoria = context.Categorias.Find(id);
+            context.Categorias.Remove(categoria);
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public ActionResult Details(long id)
+        public ActionResult Details(long? id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
         }
     }
 }
--- TrainningMVC/Controllers/FabricantesController.cs
+++ TrainningMVC/Controllers/FabricantesController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TrainningMVC.Contexts;
+using TrainningMVC.Models;
 
 namespace TrainningMVC.Controllers
 {
@@ -16,5 +19,94 @@ namespace TrainningMVC.Controllers
         {
             return View(context.Fabricantes.OrderBy(c => c.Nome));
         }
+
+
+        //CREATE: Fabricates
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //EDIT: Fabricantes
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new
+                    HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante==null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(fabricante).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fabricante);
+
+        }
+
+        //Details: Fabricantes
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        //Delete: Fabricantes
+        public ActionResult Delete(long? id)
+        {
+            if (id ==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            context.Fabricantes.Remove(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+       
     }
 }
